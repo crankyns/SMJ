7ed4b17a872a55e487a0bfc2e0d61bc297b741b5
linescapi <linescapi@gmail.com>
2022-01-05 21:46:08 +0100

    black html

--- src/components/HomePage/MainSection/CounterCard.jsx
+++
@@ -1,8 +0,0 @@
-export const CounterCard = ({ numberCount, cardText }) => {
-  return (
-    <div className={"list-item"}>
-      <p className={"item-number"}>{numberCount}</p>
-      <p className={"`item-text"}>{cardText}</p>
-    </div>
-  );
-};
